f27ad4875947134130f6c0575eb74e77f67a38d5
w3dg <51162936+w3dg@users.noreply.github.com>
2022-09-02 15:45:57 +0530

    :sparkles: Change to absolute path for script

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 const { readFileSync } = require("fs");
-const fortuneData = readFileSync("./fortune.txt", "utf-8");
+const path = require("path");
+const dirPath = path.join(__dirname, "/fortune.txt");
+const fortuneData = readFileSync(dirPath, "utf-8");
 const fortunes = fortuneData.split("%\r\n");
 const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)].trim();
 console.log(randomFortune);
